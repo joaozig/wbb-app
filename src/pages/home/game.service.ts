67c4adc5b6268c337da6fa4c503ba0e4591604c6
João Ricardo Bastos <joaoricardobastos@gmail.com>
2017-10-08 10:54:11 -0300

    add Game service

---
+++ src/pages/home/game.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+import { API_URL } from '../../app/constants';
+
+@Injectable()
+export class GameService {
+
+  constructor(public http: Http, public storage: Storage) { }
+
+  getChampionships(sportId, userGroupId): Promise<any> {
+    return new Promise((resolve, reject) => {
+      let url = API_URL + '/includes/inc.championship.php';
+      url += '?sportId='+sportId+'&groupId='+userGroupId;
+
+      this.http.get(url).map(res => res.json()).subscribe(response => {
+        if(response) {
+          resolve(response.championship);
+        } else {
+          resolve([]);
+        }
+      }, (error) => {
+        reject('Não foi possível recuperar os campeonatos.');
+      });
+    });
+  }
+}
