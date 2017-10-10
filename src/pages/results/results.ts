842d7992ded15d2e15fd1acedb9709b263ed5d15
João Ricardo Bastos <joaoricardobastos@gmail.com>
2017-10-09 23:58:16 -0300

    implement results page

--- src/pages/results/results.ts
+++ src/pages/results/results.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 import { Util } from '../../app/util';
 
@@ -15,9 +15,15 @@ export class ResultsPage {
   results: any;
   initialDate: any;
   finalDate: any;
+  formattedInitialDate: any;
+  formattedFinalDate: any;
+  pesquisar: any = '';
+  shownGroup: any;
+  loading: Boolean;
 
   constructor(
     public navCtrl: NavController,
+    public alertCtrl: AlertController,
     public resultsService: ResultsService) {
 
     this.currentDate();
@@ -28,14 +34,47 @@ export class ResultsPage {
     this.setDates(date);
   }
 
+  prevDate() {
+    console.log(this.pesquisar);
+  	this.setDates(this.initialDate.setDate(this.initialDate.getDate() - 2));
+  }
+
+  nextDate() {
+		this.setDates(this.finalDate.setDate(this.finalDate.getDate() + 2));
+  }
+
   setDates(date) {
+    this.loading = true;
     this.initialDate = Util.getMonday(date);
     this.finalDate = Util.getSunday(date);
+
+    this.formattedInitialDate = Util.formatDate(this.initialDate);
+    this.formattedFinalDate = Util.formatDate(this.finalDate);
+
     this.loadResults();
   }
 
   loadResults() {
-    console.log(this.initialDate)
-    console.log(this.finalDate)
+		var initialDate = Util.formatFilterDate(this.initialDate);
+		var finalDate = Util.formatFilterDate(this.finalDate);
+    this.resultsService.getResults(initialDate, finalDate)
+      .then((response) => {
+				this.shownGroup = new Array(response.length);
+				for(var i = 0; i < response.length; i++){
+					this.shownGroup[i] = true;
+				}
+        this.results = response;
+        this.loading = false;
+      }, (errorMessage) => {
+        this.alertCtrl.create({
+          title: 'Algo falhou :(',
+          message: errorMessage,
+          buttons: ['OK']
+        }).present();
+      });
   }
+
+	toggleGroup(index) {
+    this.shownGroup[index] = !this.shownGroup[index];
+  };
 }
