00aad48d7846b5b1b8de766628109118f757147b
João Ricardo Bastos <joaoricardobastos@gmail.com>
2017-10-12 00:11:30 -0300

    add getResume method

--- src/pages/financial/financial.service.ts
+++ src/pages/financial/financial.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
+import { LoginService } from '../login/login.service';
 import { API_URL } from '../../app/constants';
 
 @Injectable()
 export class FinancialService {
 
-  constructor(public http: Http) { }
+  constructor(public http: Http, public loginService: LoginService) { }
 
   getBets(initialDate, finalDate, sellerId, groupId): Promise<any> {
     return new Promise((resolve, reject) => {
@@ -19,6 +20,21 @@ export class FinancialService {
         resolve(response);
       }, (error) => {
         reject('Não foi possível recuperar o financeiro.');
+      });
+    });
+  }
+
+  getResume(initialDate, finalDate): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.loginService.getLoggedUser().then((user) => {
+        let url = API_URL + '/includes/inc.financial.manager.php';
+        url += '?dataIni='+initialDate+'&dataFim='+finalDate+'&seller='+user.id;
+
+        this.http.get(url).map(res => res.json()).subscribe(response => {
+          resolve(response);
+        }, (error) => {
+          reject('Não foi possível recuperar o resumo financeiro.');
+        });
       });
     });
   }
